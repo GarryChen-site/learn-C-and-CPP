5278d5a9011900fad331f8223d49875d418e9ceb
GarryChen <chenymj23@gmail.com>
2020-11-26 23:10:55 +0800

    init

---
+++ data_structures/carrray.h
@@ -0,0 +1,21 @@
+
+/*
+ * Header for Array in C
+ * 
+ */
+
+// 预编译一次
+#pragma once
+// 如果是cpp代码，那么加入 extern “C”{ 和 } 并处理其中代码
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
