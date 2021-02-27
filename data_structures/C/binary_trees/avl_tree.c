33ec1ef9b6805143f5ee6263d3e881026291d26a
GarryChen <chenymj23@gmail.com>
2021-02-27 21:12:01 +0800

    add rotate

--- data_structures/C/binary_trees/avl_tree.c
+++ data_structures/C/binary_trees/avl_tree.c
@@ -51,6 +51,7 @@ int heightDiff(avlNode *node)
         return (nodeHeight(node->left) - nodeHeight(node->right));
 }
 
+// 给定avlNode的最小值
 avlNode *minNode(avlNode *node)
 {
     avlNode *temp = node;
@@ -61,6 +62,52 @@ avlNode *minNode(avlNode *node)
 
 void printAVL(avlNode *node, int level)
 {
+    int i;
+    if(node != NULL)
+    {
+        printAVL(node->right, level + 1);
+        printf("\n\n");
 
+        for(i=0; i<level;i++)
+        {
+            printf("\t");
+        }
+        printf("%d",node->key);
+        printAVL(node->left, level+1);
+    }
 }
 
+avlNode *leftRotate(avlNode *z)
+{
+    avlNode *y = z->right;
+    avlNode *T3 = y->left;
+
+    y->left = z;
+    z->right = T3;
+
+    z->height = (max(nodeHeight(z->left),nodeHeight(z->right)) + 1);
+    y->height = (max(nodeHeight(y->left),nodeHeight(y->right)) + 1);
+
+    return y;
+}
+// 考虑相对位置
+// insert 3 2 1
+avlNode *rightRotate(avlNode *z)
+{
+    avlNode *y = z->left;
+    avlNode *T3 = y->right;
+
+    y->right = z; // 是y的right等于z，不是z等于y->right
+    z->left = T3;
+
+    z->height = (max(nodeHeight(z->left),nodeHeight(z->right)) + 1);
+    y->height = (max(nodeHeight(y->left),nodeHeight(y->right)) + 1);
+
+    return y;
+}
+
+//insert 3 1 2
+avlNode *LeftRightRotate(avlNode *z)
+{
+
+}
