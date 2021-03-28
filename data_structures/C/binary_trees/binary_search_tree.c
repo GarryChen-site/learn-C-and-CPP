ee2b980c4683d697358a686d718a858c9ff0a05c
GarryChen <chenymj23@gmail.com>
2021-03-28 21:14:30 +0800

    delete to be continue

--- data_structures/C/binary_trees/binary_search_tree.c
+++ data_structures/C/binary_trees/binary_search_tree.c
@@ -69,3 +69,113 @@ int find(node *root, int data)
         return 0;
     }
 }
+
+int height(node *root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+
+    else
+    {
+        int right_h = height(root->right);
+        int left_h = height(root->left);
+
+        // The final height is the height of the greatest subtree(left or right)
+        // plus 1 (which is the root's level)
+        if(right_h > left_h)
+        {
+            return (right_h + 1);
+        }
+        else
+        {
+            return (left_h + 1);
+        }
+    }
+}
+
+void purge(node *root)
+{
+    if(root != NULL)
+    {
+        if(root->left != NULL)
+        {
+            purge(root->left);
+        }
+        if(root->right != NULL)
+        {
+            purge(root->right);
+        }
+        free(root);
+        root = NULL; // reset pointer
+    }
+}
+
+void inOrder(node *root)
+{
+    if(root != NULL)
+    {
+        inOrder(root->left);
+        printf("\t[ %d ]\t",root->data);
+        inOrder(root->right);
+    }
+}
+/**
+ * 返回值是调整后的root
+ */
+node *delete(node *root, int data)
+{
+    if(root == NULL)
+    {
+        return root;
+    }
+    else if(data > root->data)
+    {
+        root->right = delete(root->right,data);
+    }
+    else if(data < root->data)
+    {
+        root->left = delete(root->left,data);
+    }
+    // 遍历到某个节点等于需要删除的data
+    else if (data == root->data)
+    {
+        if((root->left == NULL) && (root->right == NULL))
+        {
+            // Case 1: the root has no leaves, remove the node
+            free(root);
+            return NULL;
+        }
+        else if(root->left == NULL)
+        {
+            // Case 2: the root has one leaf, make the lead the new root and
+            // remove
+            node *tmp = root;
+            root = root->right;
+            free(tmp);
+            return root;
+        }
+        else if(root->right == NULL)
+        {
+            node *tmp = root;
+            root = root->left;
+            free(tmp);
+            return root;
+        }
+        else
+        {
+            // todo 
+            // Case 3: the root has 2 leaves, find the greatest key in the left
+            // subtree and switch with root's
+
+            // finds the biggest node in the left branch
+            node *tmp = getMax(root->left);
+
+            // sets the data of this node equal to the data of the biggest node
+            root->data = tmp->data;
+            root->left = delete(root->left, tmp->data);
+        }
+    }
+    return root;
+}
